cb9ad8bfb4f3b28f765978722c360ba197f7444f
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-09 02:23:15 +0530

    Create RubberDuck.java

---
+++ RubberDuck.java
@@ -0,0 +1,12 @@
+public class RubberDuck extends Duck{
+    public RubberDuck(){
+        quackBehavior=new Squeak();
+        swimBehaviour=new Floating();
+        flyBehaviour=new Notfly();
+    }
+    @Override
+    public void display() {
+        System.out.println("  I am rubber duck");
+
+    }
+}
