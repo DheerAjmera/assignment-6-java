bfa281fe968a2dc0969785efba4240a980141300
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-09 02:16:34 +0530

    setting the quack behaviour

--- Duck.java
+++ Duck.java
@@ -2,3 +2,7 @@ abstract public class Duck {
     QuackBehavior quackBehavior;
     SwimBehaviour swimBehaviour;
     FlyBehaviour flyBehaviour;
+    
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
