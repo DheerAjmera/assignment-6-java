a805eee175e80be62ddd69593ed0f60defbeb973
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-09 02:20:53 +0530

    Create MallardDuck.java

---
+++ MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck{
+    public MallardDuck(){
+        quackBehavior=new Quack();
+        swimBehaviour=new Swimming();
+        flyBehaviour = new Fly();
+    }
+    @Override
+    public void display() {
+        System.out.println("I am mallard duck");
+    }
+
+}
