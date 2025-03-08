0f50c11945d004ab327caabc28560f59032fd1d9
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-09 02:22:51 +0530

    Create RedHead.java

---
+++ RedHead.java
@@ -0,0 +1,13 @@
+public class RedHead extends Duck{
+    public RedHead(){
+        quackBehavior=new Quack();
+        swimBehaviour=new Swimming();
+        flyBehaviour = new Fly();
+    }
+    @Override
+    public void display() {
+        System.out.println("I am redhead duck");
+    }
+
+
+}
